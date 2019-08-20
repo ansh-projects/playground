2f29d5070d1586e866c83d7c24b190869afc4c66
Ansh Shukla <shuklaansh96@gmail.com>
2019-08-20 02:04:20 -0700

    Menu

--- Game/js/minesweeper.js
+++ Game/js/minesweeper.js
@@ -1,21 +1,75 @@
-var c = document.getElementById("mineField");
-var ctx;
+class Cell {
+    constructor(height, width) {
+        var texture;                     /* Texture for displaying the cell */
+        var w;                           /* int for width        */
+        var h;                           /* int for height       */
+        var x;                           /* int for x position   */
+        var y;                           /* int for y position   */
+        var proximity;                   /* int for proximity    */
+        var snooped;                     /* bool for if the Cell has been visited  */
+        var is_bomb;                     /* bool for if the Cell is a bomb */
+        var is_flag;                     /* bool for if the Cell is flagged */
+        var neighbours = new Array[8];   /* Cell vector array for adjaent neighbours */
+    };  
+}/* Struct for a Cell */
 
-setInterval(function() {
-    update();
-    render();
-}, 1000);
+var elems;
 
-function update() {
-    c.getAttribute('width') = window.innerWidth;
-    c.getAttribute('height') = window.height;
-    ctx = c.getContext("2d");
-    ctx.moveTo(0, 0);
-    ctx.lineTo(200, 100);
-    ctx.stroke();
+function loaded(){
+    elems = {
+        menu : document.getElementById('menu'),
+        container : document.getElementById('container'),
+        statusBar : {
+            bombNums : document.getElementById('numBombs'),
+            title    : document.getElementById('title'),
+            resetBtn : document.getElementById('resetBtn'),
+            moveNums : document.getElementById('numMoves'),
+        },
+        modeBtns : [
+            document.getElementById('easyModeBtn'),
+            document.getElementById('mediumModeBtn'),
+            document.getElementById('hardModeBtn')
+        ]
+    }
+    for(var i in elems.modeBtns){
+        AttachEvent(elems.modeBtns[i], "click", start);
+        AttachEvent(elems.modeBtns[i], "mouseover", handleModeButtonHover);
+        AttachEvent(elems.modeBtns[i], "mouseleave", handleModeButtonUnHover);
+    }
+    console.log("loaded");
+    console.log(elems);
 }
 
-function render() {
-    ctx.clearRect(0, 0, c.getAttribute('width'), c.getAttribute('height'))
+function start(e) {
+    engine.start(e);
 }
 
+var engine = {
+    canvas : document.createElement('canvas'),
+    start : function(modeBtn) {
+        this.canvas.setAttribute("id", "gameboard");
+        this.canvas.width = elems.container.scrollWidth;
+        this.canvas.height = elems.container.scrollHeight;
+        this.context = this.canvas.getContext('2d');
+        elems.menu.style.display = "none";
+        elems.container.appendChild(this.canvas);
+    }
+}
+
+function AttachEvent(element, type, handler) {
+	if (element.addEventListener) {
+        element.addEventListener(type, handler, false);
+    } else {
+        element.attachEvent("on"+type, handler);
+    }
+}
+
+function handleModeButtonHover(event) {
+    this.children[0].classList.add("hide");
+    this.children[1].classList.remove("hide");
+}
+
+function handleModeButtonUnHover(event) {
+    this.children[0].classList.remove("hide");
+    this.children[1].classList.add("hide");
+}
