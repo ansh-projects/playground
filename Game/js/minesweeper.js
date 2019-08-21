91b21747904ee203ff9004f5af2d6fe60b731753
Ansh Shukla <shuklaansh96@gmail.com>
2019-08-20 18:11:57 -0700

    canvas to grid

--- Game/js/minesweeper.js
+++ Game/js/minesweeper.js
@@ -1,3 +1,7 @@
+window.onbeforeunload = function(event) {
+    return "Progress will be lost."
+}
+
 class Cell {
     constructor(height, width) {
         var texture;                     /* Texture for displaying the cell */
@@ -16,6 +20,7 @@ class Cell {
 var elems;
 
 function loaded(){
+    //init elems
     elems = {
         menu : document.getElementById('menu'),
         container : document.getElementById('container'),
@@ -23,6 +28,7 @@ function loaded(){
             bombNums : document.getElementById('numBombs'),
             title    : document.getElementById('title'),
             resetBtn : document.getElementById('resetBtn'),
+            flagBtn  : document.getElementById('flagBtn'),
             moveNums : document.getElementById('numMoves'),
         },
         modes : {
@@ -46,13 +52,13 @@ function loaded(){
             }
         }
     }
+    //init events
     for(var mode in elems.modes){
         AttachEvent(elems.modes[mode].button, "click", start);
         AttachEvent(elems.modes[mode].button, "mouseover", handleModeButtonHover);
         AttachEvent(elems.modes[mode].button, "mouseleave", handleModeButtonUnHover);
     }  
-    console.log("loaded");
-    console.log(elems);
+    console.log("Page Elements Loaded");
 }
 
 function start(e) {
@@ -60,14 +66,61 @@ function start(e) {
 }
 
 var engine = {
-    canvas : document.createElement('canvas'),
-    start : function(modeBtn) {
-        this.canvas.setAttribute("id", "gameboard");
-        this.canvas.width = elems.container.scrollWidth;
-        this.canvas.height = elems.container.scrollHeight;
-        this.context = this.canvas.getContext('2d');
-        elems.menu.style.display = "none";
-        elems.container.appendChild(this.canvas);
+    grid : document.createElement('div'),
+
+    game : {},
+    
+    start : function(event) {
+        this.initGame(event);
+    },
+
+    initGame : function(event) {
+        var infoID = event.target.id;
+        var mode = infoID.substring(0, infoID.length - 4);
+        switch (mode) {
+            case 'easy':
+            case 'medium':
+            case 'hard':
+                this.game.mode = elems.modes[mode];
+                break;
+            default:
+                break;
+        }
+        elems.statusBar.title.classList.add("hide");
+        elems.statusBar.resetBtn.classList.remove("hide");
+        elems.statusBar.flagBtn.classList.remove("hide");
+        this.initGrid()
+    },
+
+    initGrid : function() {
+        var rows = this.game.mode.rows;
+        var cols = this.game.mode.cols;
+        elems.menu.style.display = 'none';
+        this.grid.setAttribute("id", "gameboard");
+        elems.container.appendChild(this.grid);
+        var gridTemplateColumns = '';
+        var gridTemplateRows = '';
+        for(var i = 0; i < cols; i++){
+            gridTemplateColumns += 'auto ';
+        }
+        var gridTemplateRows = '';
+        for(var i = 0; i < rows; i++){
+            gridTemplateRows += 'auto ';
+        }
+        this.grid.style.gridTemplateColumns = gridTemplateColumns;
+        this.grid.style.gridTemplateRows = gridTemplateRows;
+        this.initCells(rows, cols);
+    },
+
+    initCells : function(rows, cols) {
+        var cell_id = 0;
+        for(var row = 0; row < rows; row++){
+            for(var col = 0; col < cols; col++){
+                var cell = document.createElement('div');
+                cell.setAttribute("class", "cell");
+                this.grid.appendChild(cell);
+            }
+        }
     }
 }
 
@@ -80,9 +133,22 @@ function AttachEvent(element, type, handler) {
 }
 
 function handleModeButtonHover(event) {
-    var target = event.target.innerHTML.toLowerCase();
-    //todo make switch
-    var bombs = elems.modes[target].bombs;
+    var mode = event.target.innerHTML.toLowerCase();
+    var bombs = elems.statusBar.bombNums.children[0].innerHTML;
+
+    switch (mode) {
+        case 'easy':
+        case 'medium':
+        case 'hard':
+            var game_mode = elems.modes[mode];
+            bombs = game_mode.bombs;
+            this.children[1].innerHTML = game_mode.rows + " × " + game_mode.cols;
+            break;
+    
+        default:
+            break;
+    }
+
     this.children[0].classList.add("hide");
     this.children[1].classList.remove("hide");
     elems.statusBar.bombNums.children[0].innerHTML = bombs;
